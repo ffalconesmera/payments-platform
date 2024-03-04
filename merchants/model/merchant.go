f6cb4ff7054b962c575f57f3180bf6d7108e601c
Fabian Falcones <fabian31falcones@gmail.com>
2024-03-04 15:08:39 -0500

    modelos de orm

---
+++ merchants/model/merchant.go
@@ -0,0 +1,8 @@
+package model
+
+type Merchant struct {
+	UUID         string `gorm:"column:pay_merch_id; unique; not null" json:"-"`
+	MerchantCode string `gorm:"column:pay_merch_code; unique; not null" json:"merchant_code"`
+	Name         string `gorm:"column:pay_merch_name" json:"name"`
+	BaseModel
+}
---
+++ merchants/model/user.go
@@ -0,0 +1,10 @@
+package model
+
+type User struct {
+	UUID         string `gorm:"column:pay_usr_id; unique; not null" json:"-"`
+	Username     string `gorm:"column:pay_usr_username; unique; not null" json:"username"`
+	Email        string `gorm:"column:pay_usr_email" json:"email"`
+	Password     string `gorm:"column:pay_usr_password" json:"password,omitempty"`
+	MerchantUUID string `gorm:"column:pay_usr_merchant_id;not null" json:"-"`
+	BaseModel
+}
